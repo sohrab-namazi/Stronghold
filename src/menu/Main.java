fbd462aa480c6fe91148e5541e6ed7df07a5dd6f
sohrab-namazi <sohrabsn7@yahoo.com>
2019-06-10 16:25:32 +0430

    the backbone of the project was created

---
+++ src/menu/Main.java
@@ -0,0 +1,19 @@
+package menu;
+
+import building.Palace;
+import level.Level;
+import network.Client;
+
+public class Main
+{
+    public static void main(String[] args) {
+        Client client = new Client();
+        Palace palace = new Palace();
+        client.palace = palace;
+        Level level = new Level(palace, 5);
+        client.palace.level = level;
+        System.out.println(palace.level.grade);
+
+    }
+}
+
